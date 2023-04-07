dbf19ade2678cced32532d0aa5bee36fd69b568d
Mason Kim <qwerty3345@naver.com>
2023-04-07 10:32:35 +0900

    Initial Commit

---
+++ StarbucksClone_SwiftUI/StarbucksClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksClone_SwiftUIApp.swift
+//  StarbucksClone_SwiftUI
+//
+//  Created by Mason Kim on 2023/04/07.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
